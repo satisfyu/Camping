bbb41ae74c1143ada7f2f1c075e506d494bbbe49
jason13official <jason13official@gmail.com>
2024-07-15 15:19:38 -0400

    setting up backpacks

--- common/src/main/java/net/satisfy/camping/registry/ObjectRegistry.java
+++ common/src/main/java/net/satisfy/camping/registry/ObjectRegistry.java
@@ -4,21 +4,26 @@ import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.Registrar;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.PushReaction;
 import net.satisfy.camping.Camping;
 import net.satisfy.camping.Util.CampingIdentifier;
 import net.satisfy.camping.Util.CampingUtil;
 import net.satisfy.camping.block.SleepingBagBlock;
+import net.satisfy.camping.item.backpack.EnderBackpackItem;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public class ObjectRegistry {
+
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Camping.MODID, Registries.ITEM);
     public static final Registrar<Item> ITEM_REGISTRAR = ITEMS.getRegistrar();
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Camping.MODID, Registries.BLOCK);
@@ -26,15 +31,22 @@ public class ObjectRegistry {
 
     public static final Map<String, RegistrySupplier<Block>> SLEEPING_BAGS = new HashMap<>();
 
-
     public static final String[] colors = {
             "white", "light_gray", "gray", "black", "red", "orange", "yellow", "lime", "green", "cyan", "light_blue", "blue", "purple", "magenta", "pink", "brown"
     };
 
+    public static final RegistrySupplier<Block> ENDER_BACKPACK_BLOCK = registerWithoutItem("ender_backpack", () -> new Block(BlockBehaviour.Properties.copy(Blocks.RED_WOOL)));;
+
     static {
+
+        // sleeping bags
         for (String color : colors) {
             SLEEPING_BAGS.put(color, registerWithItem("sleeping_bag_" + color, () -> new SleepingBagBlock(DyeColor.BLUE, Block.Properties.copy(Blocks.RED_WOOL).pushReaction(PushReaction.IGNORE))));
         }
+
+
+        registerItem("ender_backpack", () -> new EnderBackpackItem(ArmorMaterials.LEATHER, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
+
         BLOCKS.register();
         ITEMS.register();
     }
