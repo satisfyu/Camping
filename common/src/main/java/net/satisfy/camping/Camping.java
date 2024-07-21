bc6070e21e58f92976bd367a4909c29ca1ead66b
satisfy <laube_marco@yahoo.de>
2024-07-21 19:35:26 +0200

    feat: curios
    No, it doesnt work

--- common/src/main/java/net/satisfy/camping/Camping.java
+++ common/src/main/java/net/satisfy/camping/Camping.java
@@ -11,14 +11,9 @@ public class Camping {
     /* To-Do List */
     // [✓] sleeping bags
     // -> no respawn point sew - how?! tried a playermixin which didnt work
-    // [✓] tents
     // [ ] backpacks (jason)
     //     ~ Small, Wanderer, Large, Ender
-    // [✓] grill
-    // [✓] swiss utility knife
     /* Config for */
-    // [ ] FoodDataMixin -> Values configurable
-    // [✓] ItemMixin -> Glint yes / no
     // [ ] SleepingBag -> MobEffect yes / no
     // [ ] More Ideas here...
 
--- forge/src/main/java/net/satisfy/camping/forge/CampingForge.java
+++ forge/src/main/java/net/satisfy/camping/forge/CampingForge.java
@@ -1,15 +1,26 @@
 package net.satisfy.camping.forge;
 
 import dev.architectury.platform.forge.EventBuses;
-import net.satisfy.camping.Camping;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.satisfy.camping.Camping;
+import net.satisfy.camping.forge.integration.CuriosBackpack;
+import net.satisfy.camping.registry.ObjectRegistry;
+import top.theillusivec4.curios.api.CuriosApi;
 
 @Mod(Camping.MODID)
 public class CampingForge {
     public CampingForge() {
-        // Submit our event bus to let architectury register our content on the right time
+        final IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         EventBuses.registerModEventBus(Camping.MODID, FMLJavaModLoadingContext.get().getModEventBus());
         Camping.init();
+        eventBus.addListener(this::setup);
+
+    }
+
+    private void setup(final FMLCommonSetupEvent evt) {
+        CuriosApi.registerCurio(ObjectRegistry.SMALL_BACKPACK_ITEM.get(), new CuriosBackpack());
     }
 }
