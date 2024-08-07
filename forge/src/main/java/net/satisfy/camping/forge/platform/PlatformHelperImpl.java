246970ea92c63d8f1dec0ed0ae22f3a44f15eab8
satisfy <laube_marco@yahoo.de>
2024-08-07 20:07:44 +0200

    feat(fix): Issue #151
    https://github.com/satisfyu/Let-s-Do-Hub/issues/151

--- forge/src/main/java/net/satisfy/camping/forge/platform/PlatformHelperImpl.java
+++ forge/src/main/java/net/satisfy/camping/forge/platform/PlatformHelperImpl.java
@@ -1,8 +1,11 @@
 package net.satisfy.camping.platform.forge;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.satisfy.camping.item.BackpackItem;
+import net.satisfy.camping.util.CampingUtil;
 
 public class PlatformHelperImpl {
 
@@ -22,5 +25,14 @@ public class PlatformHelperImpl {
             }
         }
         return ItemStack.EMPTY;
+    }
+
+    public static void addRegenEffect(Player player, MobEffectInstance effectInstance) {
+    }
+
+    public static void setGrilled(ItemStack itemStack) {
+        CompoundTag tag = itemStack.getOrCreateTag();
+        tag.putBoolean("Grilled", true);
+        CampingUtil.Grilling.increaseFoodValue(itemStack);
     }
 }
