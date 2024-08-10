05c3fbb741482577fdf8d03c56d8c60a03459441
jason13official <jason13official@gmail.com>
2024-08-10 11:58:11 -0400

    fix: sleeping bags no longer reset spawn

--- fabric/src/main/java/net/satisfy/camping/fabric/CampingFabric.java
+++ fabric/src/main/java/net/satisfy/camping/fabric/CampingFabric.java
@@ -3,6 +3,7 @@ package net.satisfy.camping.fabric;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.loader.api.FabricLoader;
@@ -10,7 +11,9 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 import net.satisfy.camping.Camping;
+import net.satisfy.camping.block.SleepingBagBlock;
 import net.satisfy.camping.fabric.compat.trinkets.BackpackTrinket;
 import net.satisfy.camping.fabric.compat.trinkets.TrinketsCompatibility;
 import net.satisfy.camping.fabric.config.ConfigFabric;
@@ -34,6 +37,12 @@ public class CampingFabric implements ModInitializer {
         if (trinketsLoaded) {
             TrinketsCompatibility.load();
         }
+
+        EntitySleepEvents.ALLOW_SETTING_SPAWN.register((player, sleepingPos) -> {
+            boolean onClient = player.level().isClientSide;
+            BlockState blockState = player.level().getBlockState(sleepingPos);
+            return !(!onClient && blockState.getBlock() instanceof SleepingBagBlock);
+        });
     }
 
     public static boolean trinketsLoaded() {
--- forge/src/main/java/net/satisfy/camping/forge/CampingForge.java
+++ forge/src/main/java/net/satisfy/camping/forge/CampingForge.java
@@ -1,7 +1,11 @@
 package net.satisfy.camping.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.event.entity.player.PlayerSetSpawnEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -9,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.satisfy.camping.Camping;
+import net.satisfy.camping.block.SleepingBagBlock;
 import net.satisfy.camping.forge.integration.CuriosBackpack;
 import net.satisfy.camping.forge.integration.CuriosSmallBackpackRenderer;
 import net.satisfy.camping.registry.ObjectRegistry;
@@ -39,5 +44,25 @@ public class CampingForge {
 
     private void clientSetup(final FMLClientSetupEvent event) {
         CuriosRendererRegistry.register(ObjectRegistry.SMALL_BACKPACK_ITEM.get(), CuriosSmallBackpackRenderer::new);
+    }
+
+    @Mod.EventBusSubscriber(modid = Camping.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class ForgeEventsHandler {
+
+        @SubscribeEvent
+        public static void playerSetSpawn(PlayerSetSpawnEvent event)
+        {
+            Level level = event.getEntity().level();
+
+            if(event.getNewSpawn() != null)
+            {
+                Block block = level.getBlockState(event.getNewSpawn()).getBlock();
+
+                if(!level.isClientSide && block instanceof SleepingBagBlock && !event.isForced())
+                {
+                    event.setCanceled(true);
+                }
+            }
+        }
     }
 }
