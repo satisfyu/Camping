9675941faadd78d3cf5bd93a1869959fcea35f9e
satisfy <laube_marco@yahoo.de>
2024-10-19 09:27:08 +0200

    feat(fix): Issue #253
    https://github.com/satisfyu/Let-s-Do-Hub/issues/253

--- fabric/src/main/java/net/satisfy/camping/fabric/compat/trinkets/BackpackTrinket.java
+++ fabric/src/main/java/net/satisfy/camping/fabric/compat/trinkets/BackpackTrinket.java
@@ -4,68 +4,63 @@ import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
 import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.satisfy.camping.item.BackpackItem;
 import net.satisfy.camping.item.EnderpackItem;
 
+import java.util.Map;
 import java.util.Optional;
 
 public class BackpackTrinket implements Trinket {
 
     @Override
     public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        final Optional<TrinketComponent> component = TrinketsApi.getTrinketComponent(entity);
-
-        component.ifPresent(trinketComponent -> trinketComponent.getInventory().forEach((s, stringTrinketInventoryMap) ->
-                stringTrinketInventoryMap.forEach((s1, trinketInventory) -> {
-                    trinketInventory.removeItem(slot.index(), 1);
-                    trinketInventory.markUpdate();
-                })
-        ));
     }
 
     @Override
     public boolean canEquip(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        return !alreadyEquippedBackpack((Player) entity);
-    }
-
-    @Override
-    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
         if (!(entity instanceof Player player)) {
-            return;
+            return false;
         }
-
-        if (alreadyEquippedBackpack(player)) {
-            ItemStack chestItemStack = player.getItemBySlot(EquipmentSlot.CHEST);
-            chestItemStack.isEmpty();
-        }
+        return !alreadyEquippedBackpack(player);
     }
 
+    /**
+     * Checks if the player has a BackpackItem or EnderpackItem already equipped
+     * in any armor or trinket slots.
+     *
+     * @param player The player to check for equipped backpacks.
+     * @return True if a backpack or enderbag is already equipped; otherwise, false.
+     */
     public static boolean alreadyEquippedBackpack(Player player) {
-        boolean[] alreadyEquipped = {false};
 
-        player.getArmorSlots().forEach(itemStack -> {
+        for (ItemStack itemStack : player.getArmorSlots()) {
             if (itemStack.getItem() instanceof BackpackItem || itemStack.getItem() instanceof EnderpackItem) {
-                alreadyEquipped[0] = true;
-            }
-        });
 
-        Optional<TrinketComponent> component = TrinketsApi.getTrinketComponent(player);
-        component.ifPresent(trinketComponent -> trinketComponent.getInventory().forEach((s, stringTrinketInventoryMap) ->
-                stringTrinketInventoryMap.forEach((s1, trinketInventory) -> {
+                return true;
+            }
+        }
+
+
+        Optional<TrinketComponent> componentOptional = TrinketsApi.getTrinketComponent(player);
+        if (componentOptional.isPresent()) {
+            TrinketComponent component = componentOptional.get();
+            Map<String, Map<String, dev.emi.trinkets.api.TrinketInventory>> inventoryMap = component.getInventory();
+
+            for (Map<String, dev.emi.trinkets.api.TrinketInventory> trinketGroup : inventoryMap.values()) {
+                for (dev.emi.trinkets.api.TrinketInventory trinketInventory : trinketGroup.values()) {
                     for (int i = 0; i < trinketInventory.getContainerSize(); i++) {
                         ItemStack trinketStack = trinketInventory.getItem(i);
-                        if (trinketStack.getItem() instanceof BackpackItem || trinketStack.getItem() instanceof EnderpackItem) {
-                            alreadyEquipped[0] = true;
-                            break;
+                        if (trinketStack != null && (trinketStack.getItem() instanceof BackpackItem || trinketStack.getItem() instanceof EnderpackItem)) {
+
+                            return true;
                         }
                     }
-                })
-        ));
-
-        return alreadyEquipped[0];
+                }
+            }
+        }
+        return false;
     }
 }
