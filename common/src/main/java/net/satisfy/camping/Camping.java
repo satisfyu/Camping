cf4816463145bd4f3b551b78a590d8ddf9049a9f
jason13official <jason13official@gmail.com>
2025-02-13 19:07:24 -0500

    add ideas

--- common/src/main/java/net/satisfy/camping/Camping.java
+++ common/src/main/java/net/satisfy/camping/Camping.java
@@ -4,6 +4,22 @@ import net.minecraft.resources.ResourceLocation;
 
 public class Camping {
 
+    // todo checklist
+    /*
+    * 1. Walking Stick - faster walking
+    * 2. Coffee Pot & Cofee Pot Stand
+    * 3. New Campfire models
+    * 4. Marshmallow on a stick
+    * 5. campfires turn off after after configurable amount of time / during rain
+    * 6. campfires have a chance to ignite wood around them
+    * 7. twist bread / bread on a stick
+    * 8. New grill variants
+    * 9. fire pit
+    * 10. pillager tent variant
+    * 11. park bench/table set
+    * 12. mosquito rare chance during the daytime, small swarms
+    */
+
     public static void init() {}
 
     public static ResourceLocation identifier(String path) {
