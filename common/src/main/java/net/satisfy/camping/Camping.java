c68757cea83989960645df8f8a9963a2d8fdd358
jason13official <jason13official@gmail.com>
2025-03-12 14:49:55 -0400

    update todo checklist, again

--- common/src/main/java/net/satisfy/camping/Camping.java
+++ common/src/main/java/net/satisfy/camping/Camping.java
@@ -6,7 +6,7 @@ public class Camping {
 
     // todo checklist
     /*
-    * 1.  DONE- Walking Stick
+    * 1.  DONE- Walking Stick - faster walking
     * 2.  Coffee Pot & Cofee Pot Stand
     * 3.  New Campfire models
     * 4.  Marshmallow on a stick
@@ -20,8 +20,6 @@ public class Camping {
     * 12. mosquito rare chance during the daytime, small swarms
     * 13. hotkey for rolling out the sleeping bag when using the large backpack
     */
-
-    // 1. DONE Walking Stick - faster walking
 
     public static void init() {}
 
