57f2939202d51f745a59eb3ad39d3ebdff41d157
jason13official <jason13official@gmail.com>
2025-03-12 14:45:27 -0400

    update checklist

--- common/src/main/java/net/satisfy/camping/Camping.java
+++ common/src/main/java/net/satisfy/camping/Camping.java
@@ -6,17 +6,19 @@ public class Camping {
 
     // todo checklist
     /*
-    * 2. Coffee Pot & Cofee Pot Stand
-    * 3. New Campfire models
-    * 4. Marshmallow on a stick
-    * 5. campfires turn off after after configurable amount of time / during rain
-    * 6. campfires have a chance to ignite wood around them
-    * 7. twist bread / bread on a stick
-    * 8. New grill variants
-    * 9. fire pit
+    * 1.  DONE- Walking Stick
+    * 2.  Coffee Pot & Cofee Pot Stand
+    * 3.  New Campfire models
+    * 4.  Marshmallow on a stick
+    * 5.  campfires turn off after after configurable amount of time / during rain
+    * 6.  campfires have a chance to ignite wood around them
+    * 7.  twist bread / bread on a stick
+    * 8.  New grill variants
+    * 9.  fire pit
     * 10. pillager tent variant
     * 11. park bench/table set
     * 12. mosquito rare chance during the daytime, small swarms
+    * 13. hotkey for rolling out the sleeping bag when using the large backpack
     */
 
     // 1. DONE Walking Stick - faster walking
