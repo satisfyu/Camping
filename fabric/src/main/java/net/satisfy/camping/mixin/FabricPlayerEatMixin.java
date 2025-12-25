1ad886c42d62f8f19d4f4992434e8d4d79db15aa
voxu-git <johnpaulcarney727@gmail.com>
2025-12-25 19:50:05 +0800

    Fixed grilled food stats displaying improperly
    Fixed a bug where eating grilled food would give max nutrition and saturation instead of a 25% increase.
    
    Fixed a bug where nutrition and saturation displayed incorrectly in mods like Appleskin.

--- fabric/src/main/java/net/satisfy/camping/mixin/FabricPlayerEatMixin.java
+++ fabric/src/main/java/net/satisfy/camping/mixin/FabricPlayerEatMixin.java
@@ -17,7 +17,7 @@ public abstract class FabricPlayerEatMixin {
     private void camping$eat(Level level, ItemStack stack, FoodProperties props, CallbackInfoReturnable<ItemStack> cir) {
         if (!FabricCampingConfig.enableGrilling || !GrillingUtil.isGrilled(stack)) return;
         Player self=(Player)(Object)this;
-        GrillingUtil.FoodValue extra=GrillingUtil.getAdditionalFoodValue(stack);
-        self.getFoodData().eat((int)(props.nutrition()*1.25)+extra.nutrition(), props.saturation()*1.25F+extra.saturationModifier());
+        GrillingUtil.FoodValue extra = GrillingUtil.getAdditionalFoodValue(stack);
+        self.getFoodData().eat(extra.nutrition(), extra.saturationModifier());
     }
 }
